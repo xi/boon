6cd849b26fc66795b293f021cd18f304d0047d26
Tobias Bengfort <tobias.bengfort@posteo.de>
2020-10-25 09:20:48 +0100

    mv package information to setup.cfg

--- setup.py
+++ setup.py
@@ -2,25 +2,4 @@
 
 from setuptools import setup
 
-README = open('README.md').read()
-
-
-setup(
-    name='boon',
-    version='0.0.0',
-    description='unix terminal framework',
-    long_description=README,
-    url='https://github.com/xi/boon',
-    author='Tobias Bengfort',
-    author_email='tobias.bengfort@posteo.de',
-    py_modules=['boon'],
-    license='MIT',
-    classifiers=[
-        'Environment :: Console :: Curses',
-        'Intended Audience :: Developers',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: User Interfaces',
-    ],
-)
+setup()
